745757d58def1eb9259214642793a75e411a76e1
jeffson66 <linj3@rose-hulman.edu>
2020-01-28 10:38:13 -0500

    Register

---
+++ src/pillsite/RegisterListener.java
@@ -0,0 +1,14 @@
+package pillsite;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegisterListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
